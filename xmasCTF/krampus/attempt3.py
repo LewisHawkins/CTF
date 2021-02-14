487275cad851b57fe5294cfa8fdd691ff77b2777
lh5g19 <lh5g19@soton.ac.uk>
2021-02-14 21:58:05 +0000

    Added the first CTF I ever did, xmas-ctf from decemener 2020.

---
+++ xmasCTF/krampus/attempt3.py
@@ -0,0 +1,79 @@
+# fuck dis
+
+mydick = {
+    "a":"sum((sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()))),sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))))))))),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))",
+    "b":"sum((sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()))),sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))))))))),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))",
+    "c":"sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))",
+    "d":"sum((sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))))))))),hash(())//hash(())))",
+    "e":"sum((sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))))))))),hash(())//hash(()),hash(())//hash(())))",
+    "f":"sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))",
+    "g":"sum((sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))))))))),hash(())//hash(())))",
+    "h":"sum(range(sum((hash(())//hash(()),hash(())//hash(()))),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))",
+    "i":"sum(range(sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))",
+    "j":"sum((hash(())//hash(()),sum(range(sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))))",
+    "k":"sum((hash(())//hash(()),hash(())//hash(()),sum(range(sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))))",
+    "l":"sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),sum(range(sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))))",
+    "m":"sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),sum(range(sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))))",
+    "n":"sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),sum(range(sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))))",
+    "o":"sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),sum(range(sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))))",
+    "p":"sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),sum(range(sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))))",
+    "q":"sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),sum(range(sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))))",
+    "r":"sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))),sum((hash(())//hash(()),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))))",
+    "s":"sum((sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))),sum((hash(())//hash(()),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))))))))))),hash(())//hash(())))",
+    "t":"sum((sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))),sum((hash(())//hash(()),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))))))))))),hash(())//hash(()),hash(())//hash(())))",
+    "u":"sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))),sum((hash(())//hash(()),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))))",
+    "v":"sum((sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))),sum((hash(())//hash(()),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))))))))))),hash(())//hash(())))",
+    "w":"sum(range(sum((hash(())//hash(()),hash(())//hash(()))),sum((hash(())//hash(()),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))))",
+    "x":"sum(range(sum((hash(())//hash(()),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))))",
+    "y":"sum((sum(range(sum((hash(())//hash(()),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))))))))))),hash(())//hash(())))",
+    "z":"sum((sum(range(sum((hash(())//hash(()),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))))))))))),hash(())//hash(()),hash(())//hash(())))",
+    "_":"sum((sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()))),sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))))))))),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))",
+    ".":"sum((sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))))))),hash(())//hash(())))",
+#    "\":
+    "(":"sum((sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))),sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))))))),hash(())//hash(())))",
+#    ")":"sum((sum(iter(range(sum(set()),sum((sum(iter(range(sum(set()),sum((hash(str(sum(set())))//hash(str(sum(set()))),hash(str(sum(set())))//hash(str(sum(set()))),hash(str(sum(set())))//hash(str(sum(set()))),hash(str(sum(set())))//hash(str(sum(set())))))))),sum((hash(str(sum(set())))//hash(str(sum(set()))),hash(str(sum(set())))//hash(str(sum(set()))),hash(str(sum(set())))//hash(str(sum(set())))))))))),sum((hash(str(sum(set())))//hash(str(sum(set()))),hash(str(sum(set())))//hash(str(sum(set()))),hash(str(sum(set())))//hash(str(sum(set()))),hash(str(sum(set())))//hash(str(sum(set()))),hash(str(sum(set())))//hash(str(sum(set())))))))",
+    ")":"c",
+    "@":"ascii(())",
+    "[":"sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()))),sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))))",
+    "]":"sum((sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()))),sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))))))))),hash(())//hash(()),hash(())//hash(())))",
+    ",":"sum(range(sum((hash(())//hash(()),hash(())//hash(()))),sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))",
+    " ":"min(str(vars()))",
+#    "+":
+    "'":"sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))),sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))))))",
+    "{":"sum((sum(range(sum((hash(())//hash(()),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))))))))))),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))",
+    "}":"sum((sum(range(sum((hash(())//hash(()),sum(range(sum(range(sum((hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()))))))))))),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(()),hash(())//hash(())))"
+#    "=":
+#    "0":
+#    "1":
+#    "2":
+#    "3":
+#    "4":
+#    "5":
+#    "6":
+#    "7":
+#    "8":
+#    "9":
+}
+
+wanted = input("Enter string: ")
+
+command = ""
+command = 'getattr(str,min(vars(str)))('*(len(wanted)-1)
+one = True
+for letter in wanted:
+    if letter == ' ' or letter == '@' or letter == ')':
+        if one == True:
+            one = False
+            command += ','
+        else:
+            command += mydick[letter] + '),'
+
+    else:
+        if one == True:
+            one = False
+            command += 'chr(' + mydick[letter] + '),'
+        else:
+            command += 'chr(' + mydick[letter] + ')),'
+
+command = command[:-1]
+print(command)
